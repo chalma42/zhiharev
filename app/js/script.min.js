95026382f6bada7d9f8e2c42c79b6fce91dabac5
Alexandr <52231545+chalma42@users.noreply.github.com>
2022-02-09 13:03:44 +0400

    делаю модальные окна

--- app/js/script.min.js
+++ app/js/script.min.js
@@ -20,3 +20,27 @@ document.querySelector('.menu__arrow').onclick = function() {
   if(elem.classList.contains('active')) {elem.classList.remove('active');}
   else {elem.classList.add('active');}
 }
+
+//скрипты для модальных окон
+let modal = document.getElementById("myModal");
+
+let btn = document.querySelectorAll(".myBtn");
+
+btn.forEach(element => {
+  element.onclick = function() {
+    modal.style.display = "block";
+  }
+});
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
